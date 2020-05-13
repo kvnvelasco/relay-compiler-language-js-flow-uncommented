4606536e95d37e0a51d70d4e1a9aa824b622f519
Kevin Velasco <kvnvelasco@users.noreply.github.com>
2020-05-13 12:30:11 +0800

    compat with relay 9.0.0

--- src/index.js
+++ src/index.js
@@ -4,24 +4,21 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
+ *
  * @format
  */
+"use strict";
 
-'use strict';
+const RelayFlowGenerator = require("relay-compiler/lib").FlowGenerator;
 
-const RelayFlowGenerator = require('relay-compiler/lib/RelayFlowGenerator');
+const formatGeneratedModule = require("./formatGeneratedModule");
 
-const formatGeneratedModule = require('./formatGeneratedModule');
+const { find } = require("relay-compiler/lib").__internal.FindGraphQLTags;
 
-const {find} = require('relay-compiler/lib/FindGraphQLTags');
-
-import type {PluginInterface} from '../RelayLanguagePluginInterface';
-
-module.exports = (): PluginInterface => ({
-  inputExtensions: ['js', 'jsx'],
-  outputExtension: 'js',
+module.exports = () => ({
+  inputExtensions: ["js", "jsx"],
+  outputExtension: "js",
   typeGenerator: RelayFlowGenerator,
   formatModule: formatGeneratedModule,
-  findGraphQLTags: find,
+  findGraphQLTags: find
 });
